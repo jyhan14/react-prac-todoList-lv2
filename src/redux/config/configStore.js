34327d78c637c6df4e0790a4700ae4f27e0eb71b
JellyKingdom <jyhan1993@gmail.com>
2023-02-13 15:26:11 +0900

    feat: basic config for redux

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,1 +1,11 @@
 // “중앙 state 관리소" 인 Store를 만드는 설정 코드들이 있는 파일 입니다.
+
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+
+});
+const store = createStore(rootReducer);
+
+export default store;
