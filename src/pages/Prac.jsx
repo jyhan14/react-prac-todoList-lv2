61caf5084e8f40e3fdd5b7588d70f3c684b9b11c
JellyKingdom <jyhan1993@gmail.com>
2023-02-14 13:23:09 +0900

    lv3-prac: base

---
+++ src/pages/Prac.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function Prac() {
+    return (
+        <>
+            <div>
+                <h1>Button</h1>
+                <div>
+                    <button>Large Primary Button</button>
+                    <button>Medium</button>
+                    <button>Small</button>
+                </div>
+                <div>
+                    <button>Large Primary Button</button>
+                    <button>Medium</button>
+                    <button>Small</button>
+                </div>
+            </div>
+            <h1>Input</h1>
+            <form>
+                <div>
+                    <div>
+                        <label>이름</label>
+                        <input type="text"/>
+                    </div>
+                    <div>
+                        <label>가격</label>
+                        <input type="text"/>
+                    </div>
+                    <button>저장</button>
+                </div>
+            </form>
+            <div>
+                <h1>Modal</h1>
+                <div>
+                    <button>open modal</button>
+                </div>
+                <div>
+                    <button>open modal</button>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h1>Select</h1>
+                    <div>
+                        <div>
+                            <button>select버튼?</button>
+                        </div>
+                        <div>
+                            <button>select버튼? </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Prac;
