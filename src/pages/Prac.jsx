e7dce9292d9b11d5bde72db226095880fd4f0508
JellyKingdom <jyhan1993@gmail.com>
2023-02-15 16:25:42 +0900

    add modal1

--- src/pages/Prac.jsx
+++ src/pages/Prac.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function Prac() {
-
+    //--------------------------------------------------------
     //inputs
 
-    const [inputName, setInputName] = useState('');
-    const [price, setPrice]  = useState(0);
+    const [inputName, setInputName] = useState("");
+    const [price, setPrice] = useState(0);
 
     const numberComma = (e) => {
         const value = e.target.value;
-        const removedCommaValue = Number(value.replaceAll(',',""));
+        const removedCommaValue = Number(value.replaceAll(",", ""));
         setPrice(removedCommaValue.toLocaleString());
-    }
+    };
 
     const nameChangeHandler = (e) => {
         setInputName(e.target.value);
-    }
+    };
 
     const submitHandler = () => {
         const newInputs = {
@@ -24,12 +25,20 @@ function Prac() {
         };
 
         let resultString = "";
-        for(const [key, value] of Object.entries(newInputs)){
-            resultString += `${key} : ${value}\n`
+        for (const [key, value] of Object.entries(newInputs)) {
+            resultString += `${key} : ${value}\n`;
         }
 
-        alert(resultString)
-    }
+        alert(resultString);
+    };
+    //--------------------------------------------------------
+
+    //Modal, 아래 Modal component와 연결!
+    const [modal, setModal] = useState(false);
+
+    const showModal = () => {
+        setModal(true);
+    };
 
     return (
         <>
@@ -51,16 +60,19 @@ function Prac() {
                 <div>
                     <div>
                         <label>이름</label>
-                        <input type="text"
-                        value={inputName}
-                        onChange={nameChangeHandler}
+                        <input
+                            type="text"
+                            value={inputName}
+                            onChange={nameChangeHandler}
                         />
                     </div>
                     <div>
                         <label>가격</label>
-                        <input type="text"
-                        value={price}
-                        onChange={numberComma}/>
+                        <input
+                            type="text"
+                            value={price}
+                            onChange={numberComma}
+                        />
                     </div>
                     <button>저장</button>
                 </div>
@@ -68,10 +80,12 @@ function Prac() {
             <div>
                 <h1>Modal</h1>
                 <div>
-                    <button>open modal</button>
+                    <button onClick={showModal}>open modal</button>
+                    {modal && <Modal setModal={setModal} />}
                 </div>
                 <div>
-                    <button>open modal</button>
+                    <button onClick={showModal}>open modal</button>
+                    {modal && <Modal2 setModal={setModal} />}
                 </div>
             </div>
             <div>
@@ -87,9 +101,64 @@ function Prac() {
                     </div>
                 </div>
             </div>
-
         </>
-    )
+    );
+}
+
+function Modal({ setModal }) {
+    const closeModal = () => {
+        setModal(false);
+    };
+
+    return (
+        <ModalBox>
+            <ModalClose onClick={closeModal}>x</ModalClose>
+            <p>모달창 테스트!</p>
+        </ModalBox>
+    );
+}
+
+function Modal2({setModal}) {
+    const closeModal = () => {
+        setModal(false);
+    };
+
+    return (
+        <ModalBox>
+            <ModalClose onClick={closeModal}>x</ModalClose>
+            <p>모달창 테스트222</p>
+        </ModalBox>
+    );
 }
 
 export default Prac;
+
+const ModalBox = styled.div`
+    /* 모달창 크기 */
+    width: 300px;
+    height: 200px;
+
+    /* 최상단 위치 */
+    z-index: 999;
+
+    /* 중앙 배치 */
+    /* top, bottom, left, right 는 브라우저 기준으로 작동한다. */
+    /* translate는 본인의 크기 기준으로 작동한다. */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    /* 모달창 디자인 */
+    background-color: gray;
+    border: 1px solid black;
+    border-radius: 8px;
+`;
+
+const ModalClose = styled.button`
+    /* 모달창 내부 X버튼 */
+
+    position: absolute;
+    right: 10px;
+    top: 10px;
+`;
